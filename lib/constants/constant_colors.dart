e99454e6278183902bceb2c0e2aaf3e0351a008e
SouksavathPMS <kaitk452@gmail.com>
2023-05-28 14:20:44 +0700

    Create getting start page

---
+++ lib/constants/constant_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ConstantColor {
+  static const Color primaryColor = Color(0xffDC6E55);
+}
---
+++ lib/widgets/main_button.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import '../constants/constant_colors.dart';
+
+class MainButton extends StatelessWidget {
+  const MainButton({
+    Key? key,
+    required this.title,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final String title;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 43,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor:
+              MaterialStateProperty.all(ConstantColor.primaryColor),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+        ),
+        onPressed: () => onPressed(),
+        child: Text(
+          title,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
